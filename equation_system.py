0f648d406f206ee99be15f7e28027112c28af20c
cyy <cyyever@outlook.com>
2022-01-30 23:27:00 +0800

    add code

--- equation_system.py
+++ equation_system.py
@@ -1,16 +1,12 @@
-import copy
-
 import numpy
 
 from iterative_method import fixed_point_iteration
 
 
 def jacobi_method(A, b, **kwargs):
+    diagonal = A.diagonal()
+
     def f(x):
-        tmp = copy.deepcopy(x)
-        for idx, a in enumerate(x):
-            print(b[idx] - A[idx] @ x + A[idx][idx] * a)
-            tmp[idx] = (b[idx] - A[idx] @ x + A[idx][idx] * a) / A[idx][idx]
-        return tmp
+        return numpy.divide(b - A @ x + numpy.multiply(diagonal, x), diagonal)
 
     return fixed_point_iteration(f=f, x=numpy.zeros_like(b), **kwargs)
