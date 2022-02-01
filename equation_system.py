ebc8240a702c27c84056bed684cbf82ab761224b
cyy <cyyever@outlook.com>
2022-02-01 11:34:18 +0800

    add code

--- equation_system.py
+++ equation_system.py
@@ -16,20 +16,6 @@ def jacobi_method(A, b, **kwargs):
     return fixed_point_iteration(f=f, x=numpy.zeros_like(b), **kwargs)
 
 
-def gauss_seidel_method(A, b, **kwargs):
-    """Just like jabobi method, but use the most recently updated values"""
-    diagonal = A.diagonal()
-    L_U = copy.deepcopy(A)
-    numpy.fill_diagonal(L_U, 0)
-
-    def f(x):
-        for idx in range(len(x)):
-            x[idx] = (b[idx] - L_U[idx] @ x) / diagonal[idx]
-        return x
-
-    return fixed_point_iteration(f=f, x=numpy.zeros_like(b), **kwargs)
-
-
 def successive_over_relaxation_method(A, b, w, **kwargs):
     """Let ω be a real number, and define each component of the new guess xk+1 as a weighted average of ω times the Gauss–Seidel formula and 1 − ω times the current guess."""
     diagonal = A.diagonal()
@@ -42,3 +28,8 @@ def successive_over_relaxation_method(A, b, w, **kwargs):
         return x
 
     return fixed_point_iteration(f=f, x=numpy.zeros_like(b), **kwargs)
+
+
+def gauss_seidel_method(A, b, **kwargs):
+    """Just like jabobi method, but use the most recently updated values"""
+    return successive_over_relaxation_method(A, b, w=1, **kwargs)
