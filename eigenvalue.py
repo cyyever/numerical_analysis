6dd7d400d4517e3110237c0a50808bc29681cc6f
cyy <cyyever@outlook.com>
2022-02-05 01:37:29 +0800

    fix code

--- eigenvalue.py
+++ eigenvalue.py
@@ -1,18 +1,15 @@
 import numpy as np
 
+from iterative_method import fixed_point_iteration
 
-def power_method(A: np.ndarray, epsilon=0.00001, max_iteration=1000):
-    last_eigenvalue = None
-    cur_eigenvalue = None
-    v = np.random.rand(A.shape[0], 1)
-    for _ in range(max_iteration):
-        last_eigenvalue = cur_eigenvalue
+
+def power_method(A: np.ndarray, **kwargs):
+    def f(x):
+        _, v = x
         v = A @ v
-        cur_eigenvalue = v[np.argmax(np.absolute(v))]
-        v = v / cur_eigenvalue
-        if last_eigenvalue is not None and cur_eigenvalue is not None:
-            if abs(cur_eigenvalue - last_eigenvalue) < epsilon:
-                break
-    return (cur_eigenvalue, v)
+        eigenvalue = v[np.argmax(np.absolute(v))]
+        v = v / eigenvalue
+        return (eigenvalue, v)
 
-
+    v = np.random.rand(A.shape[0], 1)
+    return fixed_point_iteration(f=f, x=(None, v))
--- test/test_eigenvalue.py
+++ test/test_eigenvalue.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pytest
-
 from eigenvalue import power_method
 
 
 def test_power_method():
     A = np.asarray([[6, 5], [1, 2]])
-    max_eigenvalue, eigenvector = power_method(A)
+    max_eigenvalue, _ = power_method(A)
     assert pytest.approx(max_eigenvalue, 7)
