8e39f9f1d6f2792bf5f49695118d16fba698a1a5
cyy <cyyever@outlook.com>
2022-02-05 11:02:08 +0800

    fix code

--- eigenvalue.py
+++ eigenvalue.py
@@ -3,13 +3,13 @@ import numpy as np
 from iterative_method import fixed_point_iteration
 
 
-def power_method(A: np.ndarray, **kwargs):
-    def f(x):
-        _, v = x
+def power_method(A: np.ndarray, **kwargs) -> tuple:
+    def f(v):
         v = A @ v
-        eigenvalue = v[np.argmax(np.absolute(v))]
+        eigenvalue = np.amax(np.absolute(v))
         v = v / eigenvalue
-        return (eigenvalue, v)
+        return v
 
-    v = np.random.rand(A.shape[0], 1)
-    return fixed_point_iteration(f=f, x=(None, v))
+    v = fixed_point_iteration(f=f, x=np.random.rand(A.shape[0], 1))
+    eigenvalue = np.amax(np.absolute(v))
+    return (eigenvalue, v)
