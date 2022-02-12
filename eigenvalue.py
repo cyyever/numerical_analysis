40ee39a1f9ceeea8cfbc03b55b93ca22163bad02
cyy <cyyever@outlook.com>
2022-02-12 10:27:45 +0800

    fix code

--- eigenvalue.py
+++ eigenvalue.py
@@ -1,15 +1,36 @@
 import numpy as np
+import scipy.linalg
 
 from iterative_method import fixed_point_iteration
 
 
-def power_method(A: np.ndarray, **kwargs) -> tuple:
+def power_iteration_method(A: np.ndarray, **kwargs) -> tuple:
     def f(v):
         v = A @ v
         eigenvalue = np.amax(np.absolute(v))
         v = v / eigenvalue
         return v
 
-    v = fixed_point_iteration(f=f, x=np.random.rand(A.shape[0], 1))
-    eigenvalue = np.amax(np.absolute(v))
-    return (eigenvalue, v)
+    v = fixed_point_iteration(f=f, x=np.random.rand(A.shape[0], 1), **kwargs)
+    # use Rayleigh quotient
+    u = A @ v
+    eigenvalue = v.reshape(-1).dot(u.reshape(-1)) / (v.reshape(-1).dot(v.reshape(-1)))
+    return (eigenvalue, u / eigenvalue)
+
+
+def inverse_power_iteration_method(A: np.ndarray, shift: float = 0, **kwargs) -> tuple:
+    A = A - np.eye(A.shape[0]) * shift
+    lu, piv = scipy.linalg.lu_factor(A)
+
+    def f(v):
+        v = scipy.linalg.lu_solve((lu, piv), v)
+        eigenvalue = np.amax(np.absolute(v))
+        v = v / eigenvalue
+        return v
+
+    v = fixed_point_iteration(f=f, x=np.random.rand(A.shape[0], 1), **kwargs)
+    # use Rayleigh quotient
+    u = scipy.linalg.lu_solve((lu, piv), v)
+    eigenvalue = v.reshape(-1).dot(u.reshape(-1)) / (v.reshape(-1).dot(v.reshape(-1)))
+    eigenvalue = 1 / eigenvalue + shift
+    return (eigenvalue, u / eigenvalue)
--- test/test_eigenvalue.py
+++ test/test_eigenvalue.py
@@ -1,9 +1,16 @@
 import numpy as np
 import pytest
-from eigenvalue import power_method
+from eigenvalue import inverse_power_iteration_method, power_iteration_method
 
 
-def test_power_method():
+def test_power_iteration_method():
     A = np.asarray([[6, 5], [1, 2]])
-    max_eigenvalue, _ = power_method(A)
+    max_eigenvalue, _ = power_iteration_method(A)
     assert pytest.approx(max_eigenvalue, 7)
+
+
+def test_inverse_power_iteration_method():
+    A = np.asarray([[6, 5], [1, 2]])
+    min_eigenvalue, eigenvector = inverse_power_iteration_method(A)
+    assert pytest.approx(min_eigenvalue, 1)
+    print(eigenvector)
