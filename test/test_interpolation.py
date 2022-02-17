2e327ce1994723f42f4d065ebc8143e9af94b890
cyy <cyyever@outlook.com>
2022-02-17 23:57:17 +0800

    add code

--- test/test_interpolation.py
+++ test/test_interpolation.py
@@ -32,5 +32,4 @@ def test_chebyshev_base_points():
 
 def test_natural_cubic_spline():
     polynomials = natural_cubic_spline([(0, 3), (1, -2), (2, 1)])
-    print(polynomials[0])
     assert polynomials[0] == Polynomial([3.0, -7.0, 0.0, 2.0], [0.0] * 3)
