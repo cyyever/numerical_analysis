e9cfec28a69add4bab3218574484e305182eb0f6
cyy <cyyever@outlook.com>
2022-02-19 11:51:51 +0800

    add test

--- test/test_spline.py
+++ test/test_spline.py
@@ -1,8 +1,12 @@
-
 from polynomial import Polynomial
-from spline import natural_cubic_spline
+from spline import bezier_curve, natural_cubic_spline
 
 
 def test_natural_cubic_spline():
     polynomials = natural_cubic_spline([(0, 3), (1, -2), (2, 1)])
     assert polynomials[0] == Polynomial([3.0, -7.0, 0.0, 2.0], [0.0] * 3)
+
+
+def test_bezier_curve():
+    f = bezier_curve([(1, 1), (1, 3), (3, 3), (2, 2)])
+    assert f(0) == 1
