68b125a37acffa3c87371549f3e917fc5acdb8c5
cyy <cyyever@outlook.com>
2022-02-21 21:04:23 +0800

    add code

--- conjugate_gradient.py
+++ conjugate_gradient.py
@@ -9,7 +9,7 @@ def conjugate_gradient(A, b, **kwargs):
     """A must be positive definite"""
     assert numpy.all(A == A.T)
 
-    x = numpy.ones_like(b)
+    x = numpy.zeros_like(b)
     r = b - A @ x
     d = r
     delta = r @ r
@@ -27,6 +27,39 @@ def conjugate_gradient(A, b, **kwargs):
         old_delta = delta
         delta = r @ r
         d = r + (delta / old_delta) * d
+        return x
+
+    return fixed_point_iteration(conjugate_gradient_impl, x=x, **kwargs)
+
+
+def jacobi_preconditioner(A):
+    return numpy.diagflat(numpy.diagonal(A))
+
+
+def preconditioned_conjugate_gradient(A, b, M, **kwargs):
+    """A must be positive definite"""
+    assert numpy.all(A == A.T)
+
+    x = numpy.zeros_like(b)
+    r = b - A @ x
+    z = numpy.linalg.solve(M, r)
+    d = z
+    delta = r @ z
+
+    def conjugate_gradient_impl(x):
+        r"""
+        Implements Conjugate Gradient illustrated by
+        An Introduction to the Conjugate Gradient Method Without the Agonizing Pain
+        """
+        nonlocal r, d, delta
+        q = A @ d
+        alpha = delta / (d @ q)
+        x = x + alpha * d
+        r = r - alpha * q
+        z = numpy.linalg.solve(M, r)
+        old_delta = delta
+        delta = r @ z
+        d = z + (delta / old_delta) * d
         return x
 
     return fixed_point_iteration(conjugate_gradient_impl, x=x, **kwargs)
--- test/test_conjugate_gradient.py
+++ test/test_conjugate_gradient.py
@@ -1,6 +1,7 @@
 import numpy
 import pytest
-from conjugate_gradient import conjugate_gradient
+from conjugate_gradient import (conjugate_gradient, jacobi_preconditioner,
+                                preconditioned_conjugate_gradient)
 
 
 def test_conjugate_gradient():
@@ -11,4 +12,11 @@ def test_conjugate_gradient():
     A = -A
     b = -b
     x = conjugate_gradient(A, b)
+    assert numpy.linalg.norm(A @ x - b) == pytest.approx(0, abs=0.0000001)
+
+
+def test_preconditioned_conjugate_gradient():
+    A = numpy.array([[2.0, 2.0], [2.0, 5.0]])
+    b = numpy.array([6.0, 3.0])
+    x = preconditioned_conjugate_gradient(A, b, M=jacobi_preconditioner(A))
     assert numpy.linalg.norm(A @ x - b) == pytest.approx(0, abs=0.0000001)
