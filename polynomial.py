f4b751477945ac4bd980838f78ad5c1d5cc42861
cyy <cyyever@outlook.com>
2022-02-26 15:11:10 +0800

    add code

--- polynomial.py
+++ polynomial.py
@@ -4,14 +4,51 @@ from typing import Sequence
 
 class Polynomial:
     def __init__(self, coefficients: Sequence):
+        """the coefficients are from the lowest power to the highest power"""
         assert coefficients
-        if len(coefficients) == 1 and coefficients[0] == 0:
-            raise RuntimeError("polynomial is 0")
+        while coefficients and coefficients[-1] == 0:
+            coefficients.pop()
+        if not coefficients:
+            coefficients = [0]
         self.__coefficients: tuple = tuple(coefficients)
 
     @property
     def coefficients(self):
         return self.__coefficients
+
+    def __add__(self, other):
+        coef_size = max(len(self.coefficients), len(other.coefficients))
+        result_coefficient = list(self.coefficients) + [0] * (
+            coef_size - len(self.coefficients)
+        )
+        for i, cof in enumerate(other.coefficients):
+            result_coefficient[i] += cof
+        return Polynomial(result_coefficient)
+
+    def __sub__(self, other):
+        coef_size = max(len(self.coefficients), len(other.coefficients))
+        result_coefficient = list(self.coefficients) + [0] * (
+            coef_size - len(self.coefficients)
+        )
+        for i, cof in enumerate(other.coefficients):
+            result_coefficient[i] -= cof
+        return Polynomial(result_coefficient)
+
+    def __mul__(self, other):
+        other_coefficients = None
+        match other:
+            case Polynomial():
+                other_coefficients = other.coefficients
+            case _:
+                other_coefficients = [other]
+
+        coef_size = len(self.coefficients) + len(other.coefficients)
+        result_coefficient = [0] * coef_size
+        for i, coef in enumerate(self.coefficients):
+            for j, coef2 in enumerate(other_coefficients):
+                result_coefficient[i + j] += coef * coef2
+
+        return Polynomial(result_coefficient)
 
     def __str__(self):
         return f"coefficients:{self.coefficients}"
@@ -42,8 +79,10 @@ class Polynomial:
 class PolynomialWithBasePoint:
     def __init__(self, coefficients: Sequence, base_points: Sequence):
         assert coefficients
-        if len(coefficients) == 1 and coefficients[0] == 0:
-            raise RuntimeError("polynomial is 0")
+        while coefficients and coefficients[-1] == 0:
+            coefficients.pop()
+        if not coefficients:
+            coefficients = [0]
         self.__coefficients: tuple = tuple(coefficients)
         self.__base_points: tuple = tuple(base_points)
 
--- test/test_polynomial.py
+++ test/test_polynomial.py
@@ -5,3 +5,14 @@ def test_polynomial():
     f = Polynomial([1, 2, 3])
     assert f(1) == 6
     assert f(2) == 17
+    g = f
+    h = f + g
+    assert h(1) == f(1) * 2
+    assert h(2) == f(2) * 2
+    h = f - g
+    assert h(1) == 0
+    assert h(2) == 0
+
+    h = f * g
+    assert h(1) == f(1) * g(1)
+    assert h(2) == f(2) * g(2)
