cdc67c3477847b287967115946a5837f5c8f1cfc
cyy <cyyever@outlook.com>
2022-03-22 21:15:02 +0800

    add code

--- integration.py
+++ integration.py
@@ -17,3 +17,13 @@ def composite_trapezoid_rule(f: Callable, a: float, b: float, m: int) -> float:
     for i in range(1, m):
         res += 2 * f(a + h * i)
     return res * h / 2
+
+
+def composite_simpson_rule(f: Callable, a: float, b: float, m: int) -> float:
+    h = (b - a) / (2 * m)
+    res = f(a) + f(b)
+    for i in range(1, m + 1):
+        res += 4 * f(a + h * (2 * i - 1))
+    for i in range(1, m):
+        res += 2 * f(a + h * 2 * i)
+    return res * h / 3
--- test/test_integration.py
+++ test/test_integration.py
@@ -1,7 +1,8 @@
 import math
 
 import pytest
-from integration import composite_trapezoid_rule, simpson_rule, trapezoid_rule
+from integration import (composite_simpson_rule, composite_trapezoid_rule,
+                         simpson_rule, trapezoid_rule)
 
 
 def test_trapezoid_rule():
@@ -16,3 +17,7 @@ def test_composite_trapezoid_rule():
 
 def test_simpson_rule():
     assert simpson_rule(math.log, 1, 2) == pytest.approx(0.3858, abs=0.001)
+
+
+def test_composite_simpson_rule():
+    assert composite_simpson_rule(math.log, 1, 2,m=4) == pytest.approx(0.3863, abs=0.001)
