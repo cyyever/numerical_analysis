3e7fbbb6116d909d340ec93a2aab64fcf8dac52e
cyy <cyyever@outlook.com>
2022-03-22 14:33:51 +0800

    fix code

---
+++ differentiation.py
@@ -0,0 +1,21 @@
+from typing import Callable
+
+
+def two_point_forward_difference(f: Callable, x: float, h=None) -> float:
+    if h is None:
+        h = 0.0001
+    return (f(x + h) - f(x)) / h
+
+
+def three_point_centered_difference(f: Callable, x: float, h=None) -> float:
+    if h is None:
+        h = 0.0001
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+
+def three_point_centered_difference_for_second_derivative(
+    f: Callable, x: float, h=None
+) -> float:
+    if h is None:
+        h = 0.0001
+    return (f(x - h) - 2 * f(x) + f(x + h)) / (h**2)
---
+++ test/test_differentiation.py
@@ -0,0 +1,23 @@
+import pytest
+from differentiation import (
+    three_point_centered_difference,
+    three_point_centered_difference_for_second_derivative,
+    two_point_forward_difference)
+
+
+def test_two_point_forward_difference():
+    assert two_point_forward_difference(
+        f=lambda x: 1 / x, x=2, h=0.00000001
+    ) == pytest.approx(-1 / 4, abs=0.001)
+
+
+def test_three_point_centered_difference():
+    assert three_point_centered_difference(
+        f=lambda x: 1 / x, x=2, h=0.00000001
+    ) == pytest.approx(-1 / 4, abs=0.001)
+
+
+def test_three_point_centered_difference_for_second_derivative():
+    assert three_point_centered_difference_for_second_derivative(
+        f=lambda x: 1 / x, x=2, h=0.01
+    ) == pytest.approx(1 / 4, abs=0.001)
